97452e976ace7f0acdf69215509c57dfd39e5122
Your Name <you@example.com>
2025-12-14 20:19:19 +0400

    TASK DONE

---
+++ Controllers/SinglepageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationTASK14.Controllers
+{
+    public class SinglepageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
